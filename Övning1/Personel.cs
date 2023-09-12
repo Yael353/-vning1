dfea6ea02caefe39340a54cbd7b55e040cbf6d14
Yaser <Yaser@MSI>
2023-09-12 16:29:12 +0200

    Add project files.

---
+++ Övning1/Personel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning1
+{
+	internal class Personel
+	{
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string perSal { get; set; }
+    }
+}
---
+++ Övning1/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Övning1
+{
+	internal class Program
+	{
+
+		static List<Personel> personels = new List<Personel>();
+		static void Main(string[] args)
+		{
+			bool addPersonel = true;
+
+			while (addPersonel)
+			{
+				addPersonel = MainMenu();
+				
+			}
+
+		}
+		private static bool MainMenu()
+		{
+			Console.WriteLine("List of employees");
+			Console.WriteLine("1) Add employees");
+			Console.WriteLine("2) Show employees");
+			Console.WriteLine("0) Exit");
+
+			string result = Console.ReadLine();
+			if (result == "1")
+			{
+				personels.Add(AddPersonel());
+				return true;
+			}
+			else if (result == "2")
+			{
+				ShowEmployees(personels);
+				return true;
+			}
+			else
+			{
+                Console.WriteLine("Have a nice day!");
+				return false;
+			}
+		}
+
+		private static void ShowEmployees(List<Personel> personels)
+		{
+
+			foreach (Personel personel in personels)
+			{
+                Console.WriteLine($"First name: {personel.firstName}, " +
+					$"Last name {personel.lastName} " +
+					$"salary: {personel.perSal}");
+            }
+			
+
+		}
+
+		private static Personel AddPersonel()
+		{
+			Personel personel = new Personel();
+            Console.WriteLine("Enter your first name");
+            personel.firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            personel.lastName = Console.ReadLine();
+            Console.WriteLine("Enter your salary");
+			personel.perSal = Console.ReadLine();
+
+			return personel;
+
+			
+		}
+	}
+}
